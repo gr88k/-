a564d8fbcb1c833b646448b8f4c415e6c7beae31
gr88k <chernokozhikhivan@gmail.com>
2025-11-06 10:48:45 +0300

    commit practice 1

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,5 +1,13 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
-    print("weredwdw3")
+    val name = "Anton"
+    println("My name is $name")
+
+    var a = 1
+    a = a + 1
+    println(a)
+
+    val b = null
+    println(b)
 }
